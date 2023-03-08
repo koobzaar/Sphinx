08564e3a334e8acd18a3d420f5163744019c0b69
Bruno Trigueiro <bruno.trigueiro@outlook.com>
2023-03-08 14:57:01 -0300

    Encryption concluded

--- chaos.py
+++ chaos.py
@@ -1,10 +1,13 @@
 import numpy as np
 from convertImageToDNA import ImagemPadrao
+import matplotlib.pyplot as plt
 #from convertImageToDNA import ImagemPadrao
 
 imagem = ImagemPadrao()
 image = imagem.lerImagem("./image.jpg")
-
+print(len(image))
+print(len(image[0]))
+print(len(image[0][0]))
 [vetor_nitrogenada_r,vetor_nitrogenada_g,vetor_nitrogenada_b] = imagem.criarMatrizNitrogenada(image)
 
 codes = {'G': 0, 'A': 1, 'T': 2, 'C': 3}
@@ -59,3 +62,70 @@ permuted_seq_r = chaotic_sort(num_seq_r)
 permuted_seq_g = chaotic_sort(num_seq_g)
 permuted_seq_b = chaotic_sort(num_seq_b)
 
+# Seguindo o seguinte dicionario, transforma cada base nitrogenada em binario:  {"00": "A", "01": "C", "10": "T", "11": "G"}
+def retornaVetorBinario(vetor_nitrogenado):
+    vetor_binario = []
+    codes = {'G': '00', 'A': '01', 'T': '10', 'C': '11'}
+    for i in range(len(vetor_nitrogenado)):
+        vetor_binario.append(codes[vetor_nitrogenado[i]])
+    return vetor_binario
+
+# Agrupa a cada 4 bits 
+def retornaVetorAgrupado(vetor_binario):
+    vetor_agrupado = []
+    for i in range(0,len(vetor_binario),4):
+        vetor_agrupado.append(vetor_binario[i]+vetor_binario[i+1]+vetor_binario[i+2]+vetor_binario[i+3])
+    return vetor_agrupado
+
+# Transforma cada elemento do array em um inteiro
+def retornaVetorInteiro(vetor_agrupado):
+    vetor_inteiro = []
+    for i in range(len(vetor_agrupado)):
+        vetor_inteiro.append(int(vetor_agrupado[i],2))
+    return vetor_inteiro
+
+# Verifica se existe um valor do vetor maior que 255. Se existir, printa um erro
+def verificaVetor(vetor_inteiro):
+    for i in range(len(vetor_inteiro)):
+        if vetor_inteiro[i] > 255:
+            print("Erro")
+            break
+    return vetor_inteiro
+
+vetor_binario_r = retornaVetorBinario(permuted_seq_r)
+vetor_binario_g = retornaVetorBinario(permuted_seq_g)
+vetor_binario_b = retornaVetorBinario(permuted_seq_b)
+grouped_vector_r = retornaVetorAgrupado(vetor_binario_r)
+grouped_vector_g = retornaVetorAgrupado(vetor_binario_g)
+grouped_vector_b = retornaVetorAgrupado(vetor_binario_b)
+integer_vector_r = retornaVetorInteiro(grouped_vector_r)
+integer_vector_g = retornaVetorInteiro(grouped_vector_g)
+integer_vector_b = retornaVetorInteiro(grouped_vector_b)
+integer_vector_r = verificaVetor(integer_vector_r)
+integer_vector_g = verificaVetor(integer_vector_g)
+integer_vector_b = verificaVetor(integer_vector_b)
+
+# Cria uma matriz com os valores do vetor de cada cor correspondente. A matriz deve ter as dimensoes originais
+def retornaMatriz(vetor_r,vetor_g,vetor_b, width, height):
+    matriz = []
+    for i in range(height):
+        matriz.append([])
+        for j in range(width):
+            matriz[i].append([vetor_r[i*width+j],vetor_g[i*width+j],vetor_b[i*width+j]])
+    return matriz
+   
+
+matriz = retornaMatriz(integer_vector_r,integer_vector_g,integer_vector_b, len(image[0]), len(image))
+print(len(matriz))
+print(len(matriz[0]))
+print(len(matriz[0][0]))
+
+from PIL import Image
+
+def exibir_imagem(matriz_pixels):
+    # criar objeto de imagem a partir da matriz de pixels
+    imagem = Image.fromarray(np.uint8(matriz_pixels))
+    # armazena a imagem
+    imagem.save('imagem.png')
+    # exibir imagem
+    imagem.show()
